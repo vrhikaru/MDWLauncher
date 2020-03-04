a01bdba113867ade60cdc8f77cbec8ccb8f681ac
vrhikaru <vrhikaru@gmail.com>
2020-03-04 11:04:34 +0800

    update for auto set as default lancher

--- app/src/main/java/com/mdw/mdwlauncher/MainActivity.java
+++ app/src/main/java/com/mdw/mdwlauncher/MainActivity.java
@@ -6,9 +6,12 @@ package com.mdw.mdwlauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -16,6 +19,7 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,11 +45,30 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public static void resetPreferredLauncherAndOpenChooser(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        ComponentName componentName = new ComponentName(context, com.mdw.mdwlauncher.MainActivity.class);
+        packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+
+        Intent selector = new Intent(Intent.ACTION_MAIN);
+        selector.addCategory(Intent.CATEGORY_HOME);
+        selector.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(selector);
+
+        packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Process p;
+        try {
+             p = Runtime.getRuntime().exec("su");
+            resetPreferredLauncherAndOpenChooser(getApplicationContext());
+        }catch (IOException e) {
 
+        }
         loadApps();
         setContentView(R.layout.activity_main);
         mGrid = (GridView) findViewById(R.id.apps_list);
@@ -56,6 +79,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+            final boolean alreadyOnHome =
+                    ((intent.getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT)
+                            != Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+            if (alreadyOnHome) {
+                Log.d("whatever", "Home pressed");
+            }
+        }
+    }
 
     private void loadApps() {
         Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
